26800378cc20d7009966b8d965ada412cc2cb954
ViniciusPassoni97 <43391455+ViniciusPassoni97@users.noreply.github.com>
2020-12-27 23:14:47 -0300

    Adicionando FooterDivFree em Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,34 @@
+import { motion } from 'framer-motion';
 import React from 'react';
+import { AiOutlineArrowRight } from 'react-icons/ai';
 
-import { FooterDiv } from './style';
+import { FooterDiv, FooterDivFree } from './style';
 
 const Footer: React.FC = () => {
   return (
       <FooterDiv>
-          <h1>Footer</h1>
+          <FooterDivFree>
+            <div className='footerDivFreeInfo'> 
+                <p className='footerDivFreeInfoTitle'>Try for free!</p>
+                <p className='footerDivFreeInfoTitleWeek'>Get limited 1 week free try our features!</p>
+            </div>
+            <div className='footerDivFreeButton'> 
+              <div className='container-button'>
+                <motion.button
+                  className='container-button-img'
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >Learn More</motion.button>
+              </div>
+              <div className='container-button'>
+                <motion.button
+                  className='container-button-img'
+                  whileHover={{ scale: 1.1 }}
+                  whileTap={{ scale: 0.9 }}
+                >Request Demo <AiOutlineArrowRight className='container-button-img-logo'/></motion.button>
+              </div>
+            </div>
+          </FooterDivFree>
       </FooterDiv>
   );
 }
--- src/components/Footer/style.tsx
+++ src/components/Footer/style.tsx
@@ -1,5 +1,44 @@
 import styled from 'styled-components';
 
 export const FooterDiv = styled.div`
-    
+    border-top:1.6px solid #c5c3c3;
+`;
+export const FooterDivFree = styled.div`
+    margin-top:4rem;
+    display:flex;
+    justify-content:space-between;
+    .footerDivFreeInfo{
+        .footerDivFreeInfoTitle{
+            font-size:1.6rem;
+            font-weight:bold;
+        }
+        .footerDivFreeInfoTitleWeek{
+            margin-top:0.8rem;
+            font-size:0.8rem;
+        }
+    }
+    .footerDivFreeButton{
+        display:flex;
+        .container-button-img{
+            margin-right:2rem;
+            padding:1rem;
+            display:flex;
+            align-items:center;
+            border-radius:2rem;
+            background-color:#ffffff;
+            box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 3px 10px 0 rgba(0, 0, 0, 0.19);
+            transition:0.7s;
+        }
+        .container-button-img-logo{
+            color: #cb51db;
+            margin-left:0.6rem;
+        }
+        .container-button-img:hover{
+            .container-button-img-logo{
+                color: #fff;
+            }
+            background-color:#cb51db;
+            color:#ffffff;
+        }
+    }
 `;
